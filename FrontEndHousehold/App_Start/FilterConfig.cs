80da0b5f950e15f95b5d1d742e55052adbaad6bd
j k <janish.khanna233@gmail.com>
2019-05-29 13:39:23 -0500

    Add project files.

---
+++ FrontEndHousehold/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace FrontEndHousehold
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
